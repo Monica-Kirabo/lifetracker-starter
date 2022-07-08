233fda07ab6333b43a5d33c8d1fdb00ab75d1650
monica kirabo <kirabom@duq.edu>
2022-07-08 11:18:55 -0700

    commit here

---
+++ p3-frontend/src/App.jsx
@@ -0,0 +1,24 @@
+import { useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import Button from "@mui/material/Button";
+import Container from "@mui/material/Container";
+
+import Navbar from "./Components/Navbar";
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <Navbar />
+      <Container
+        maxWidth={false}
+        sx={{ backgroundColor: "orange", flexGrow: 1 }}
+      >
+        my container
+      </Container>
+    </>
+  );
+}
+
+export default App;
